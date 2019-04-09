0fd6628eee029402eee62f288b8d278ccdf395ec
yuqing0605 <48577102+yuqing0605@users.noreply.github.com>
2019-04-10 00:30:50 +0800

    Add files via upload

---
+++ 作業12.cpp
@@ -0,0 +1,27 @@
+//3A713221
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int number;
+	printf("¿é¤J¼Æ¦r:\n");
+	scanf("%d",&number);
+
+void show(int number) ;{
+	
+	int space;
+	for(int i=number;i>0;i--){
+	space=number-i;
+	
+	if(space>0){
+	for(int k=0;k<space;k++){
+	printf(" ");	
+	}}
+	for(int s=0;s<i;s++){
+	printf("%c",'*');
+		}
+		printf("\n");
+	}
+}}
